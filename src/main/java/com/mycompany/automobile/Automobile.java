c4443fb089dabeaa07ce66d82d8d5b6f91c0feaf
Andres Charry <ingeniero.andres.charry@gmail.com>
2024-03-11 06:39:39 -0500

    create gui

---
+++ src/main/java/com/mycompany/automobile/Automobile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automobile;
+
+import com.mycompany.automobile.gui.MainScreen;
+
+/**
+ *
+ * @author charry
+ */
+public class Automobile {
+
+    public static void main(String[] args) {
+		MainScreen main = new MainScreen();
+		main.setVisible(true);
+		main.setLocationRelativeTo(null);
+    }
+}
